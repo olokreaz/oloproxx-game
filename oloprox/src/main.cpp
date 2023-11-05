86082284e95726a4b2fed495bede06df57ad55db
olokreaz <olokreZ2005@ya.ru>
2023-11-05 11:56:20 +0300

    Update build and application configurations
    This commit adds a command to generate qrc files during the pre-build event and modifies .gitignore file to ignore qml and some related files generated. In addition, project configurations changed in oloprox.vcxproj and main.cpp to integrate QML files into the build and run process, incorporating UI implementation.

--- oloprox/src/main.cpp
+++ oloprox/src/main.cpp
@@ -7,11 +7,11 @@ import types;
 
 static bool g_bQuit = false;
 
-#include <QtWidgets/QWidget>
+#include <QtQml/QQmlApplicationEngine>
+#include <QtQml/QtQml>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
-#include <QtQml/QtQml>
-#include <QtQml/QQmlApplicationEngine>
+#include <QtWidgets/QWidget>
 
 int wmain( int, wchar_t ** )
 {
@@ -19,10 +19,19 @@ int wmain( int, wchar_t ** )
 	sys::Console::setLogLevel( spdlog::level::trace );
 	sys::Console::setConsoleTitle( "oloprox - terminal" );
 
-	QApplication app( __argc, __argv );
+	QCoreApplication::setAttribute( Qt::AA_EnableHighDpiScaling );
 
-	app . setApplicationName( "oloprox" );
+	QGuiApplication app( __argc, __argv );
 
+	QQmlApplicationEngine engine;
+	const QUrl            url( QStringLiteral( "qrc:/qml/ui/main.qml" ) );
+	QObject::connect(
+			&engine, &QQmlApplicationEngine::objectCreated,
+			&app,
+			[url] ( QObject *obj, const QUrl &objUrl ) { if ( !obj && url == objUrl ) QCoreApplication::exit( -1 ); }, Qt::QueuedConnection
+			);
+
+	engine . load( url );
 
 	return app . exec( );
 }
