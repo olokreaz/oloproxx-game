1f62b24bba4e154c832c5837ec8d219749ecf006
olokreaz <olokreZ2005@ya.ru>
2023-11-17 12:17:50 +0300

    Update IData class and add CResourceController
    Significant alterations have been made to the IData class, including new constructors, a union to support two data types, and copy and move semantics. Also implemented a new class called CResourceController which manages registered IData objects. The CResource class has been updated to align with these changes.

--- oloprox/include/oloprox/Resource.hpp
+++ oloprox/include/oloprox/Resource.hpp
@@ -1,16 +1,93 @@
 ﻿#pragma once
+#include <memory>
 #include <span>
 #include <string_view>
+#include <unordered_map>
+#include <boost/noncopyable.hpp>
+
+#include <coroutine/windows.h>
+
+import systems.files;
 
 class IData
 {
 protected:
-	std::string_view m_path;
-	std::span<uint8_t> m_data;
+	std::string m_path;
+	// std::span< uint8_t > m_data;
+
+	union data_t
+	{
+		std::span< uint8_t >   pData;
+		std::vector< uint8_t > Data;
+	}                              m_data;
+	bool                           m_bStorage { false };
+
 public:
-	IData( ) = default;
-	virtual uint8_t* data( ) const = 0;
-	virtual size_t   size( ) const = 0;
+	IData( ) : m_data { } {}
+
+	IData( std::string &path, std::vector< uint8_t > &data ) : m_path { std::move( path ) },
+								m_data { },
+								m_bStorage { true } { m_data . Data = std::move( data ); }
+
+	IData( std::string &path, std::span< uint8_t > data ) : m_path { path },
+								m_data { } { m_data . pData = data; }
+
+	virtual ~IData( ) = default;
+
+	const std::span< uint8_t >& data( ) const { return m_data . pData; }
+	const std::string&          path( ) const { return m_path; }
+
+	IData( const IData &other ) : m_path { other . m_path },
+					m_data { other . m_data },
+					m_bStorage { other . m_bStorage } {}
+
+	IData( IData &&other ) noexcept : m_path { std::move( other . m_path ) },
+					m_data { std::move( other . m_data ) },
+					m_bStorage { std::move( other . m_bStorage ) } {}
+
+	IData& operator=( const IData &other )
+	{
+		if ( this == &other ) return *this;
+		m_path     = other . m_path;
+		m_data     = other . m_data;
+		m_bStorage = other . m_bStorage;
+		return *this;
+	}
+
+	IData& operator=( IData &&other ) noexcept
+	{
+		if ( this == &other ) return *this;
+		m_path     = std::move( other . m_path );
+		m_data     = std::move( other . m_data );
+		m_bStorage = std::move( other . m_bStorage );
+		return *this;
+	}
 };
 
-template< class T > class CResource : public IData {};
+class final CResourceController : public boost::noncopyable
+{
+	static std::unordered_map< std::string, IData > m_resources;
+
+public:
+	CResourceController( )  = delete;
+	~CResourceController( ) = delete;
+
+	static void registrate( IData &data ) { m_resources[ data . path( ) ] = data; }
+};
+
+class CResource final : public IData
+{
+	CResource( std::string_view path )
+	{
+		this -> m_path        = path;
+		this -> m_data . Data = systems::files::readFile( path );
+		this -> m_bStorage    = true;
+	}
+
+	CResource( std::string_view path, std::span< uint8 > data )
+	{
+		this -> m_path         = path;
+		this -> m_data . pData = data;
+		this -> m_bStorage     = false;
+	}
+};
